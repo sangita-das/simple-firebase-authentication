6f33263b614180d3502153e39239acd8faa3a547
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2021-10-09 00:43:43 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDr18It8o5mbt_syWYonEgolAhU4khNQ1E",
+  authDomain: "simple-firebase-authenti-48a25.firebaseapp.com",
+  projectId: "simple-firebase-authenti-48a25",
+  storageBucket: "simple-firebase-authenti-48a25.appspot.com",
+  messagingSenderId: "809887206958",
+  appId: "1:809887206958:web:8255ef7f857e2ebf7ad9bf"
+};
+
+export default firebaseConfig;
